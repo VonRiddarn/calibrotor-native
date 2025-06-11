de01bbe6763c59baefcfc98d7d21416b65cfac88
VonRiddarn <vonriddarn@gmail.com>
2025-06-11 19:30:13 +0200

    refactor: enhance Home component layout and styling for improved readability

--- app/index.tsx
+++ app/index.tsx
@@ -2,16 +2,27 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import TrueWeightText from "../src/components/TrueWeightText/TrueWeightText";
 import { dateToFormatDate } from "../src/utilities/dateConverter";
+import { colors } from "../src/styles/colors";
 
 const Home = () => {
 	return (
-		<View>
+		<View style={styles.page}>
 			<TrueWeightText date={dateToFormatDate(new Date())} retrospect={7} />
-			<Text>My test text</Text>
+			<Text style={styles.inputHeader}>Todays weight</Text>
 		</View>
 	);
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	page: {
+		flex: 1,
+		alignItems: "center",
+	},
+
+	inputHeader: {
+		color: colors.text.offWhite,
+		fontSize: 40,
+	},
+});
