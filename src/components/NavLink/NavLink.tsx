f119cc0aedd021b87a8850590726edeeac4e800e
VonRiddarn <vonriddarn@gmail.com>
2025-06-11 02:49:15 +0200

    feat: add NavLink component for navigation links with active state

---
+++ src/components/NavLink/NavLink.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link, usePathname } from "expo-router";
+
+type NavLinkProps = {
+	to: string;
+	children: React.ReactNode;
+};
+
+const NavLink = ({ to, children }: NavLinkProps) => {
+	const currentPath = usePathname();
+	const active = currentPath === to;
+
+	return (
+		<Link href={to} style={[styles.link, active ? styles.active : {}]}>
+			{active ? ">" : ""}
+			{children}
+		</Link>
+	);
+};
+
+export default NavLink;
+
+const styles = StyleSheet.create({
+	link: {
+		marginVertical: 10,
+		borderBottomWidth: 1,
+	},
+	active: {
+		// Idk, color blue or something later
+	},
+});
