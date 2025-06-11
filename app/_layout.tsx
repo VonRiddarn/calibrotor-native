2db5fcee1d3b4ef1f7eb75f9926ce9ededb1d708
VonRiddarn <vonriddarn@gmail.com>
2025-06-11 02:30:35 +0200

    feat: add a work in progress - RootLayout component for main application layout

---
+++ app/_layout.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link, Slot, Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const RootLayout = () => {
+	return (
+		// This makes the stuff not overlap the native phone ui, like clock and buttons. Very cool.
+		<SafeAreaView style={styles.viewContainer} edges={["top", "bottom"]}>
+			<View style={styles.pageContainer}>
+				<Slot />
+			</View>
+			<View style={styles.footer}>
+				<Link href={"/calendar"}>Calendar</Link>
+				<Link href={"/"}>Home</Link>
+				<Text>Footer</Text>
+			</View>
+		</SafeAreaView>
+	);
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({
+	viewContainer: {
+		flex: 1,
+	},
+	pageContainer: {
+		flex: 1, // Makes the page full screen so we push down the footer
+	},
+	footer: {},
+});
