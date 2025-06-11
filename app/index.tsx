af734b6109797224f757c376612a4ab969265310
VonRiddarn <vonriddarn@gmail.com>
2025-06-11 21:56:28 +0200

    feat: add TrueWeightAccuracy component to display accuracy in Home component

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import TrueWeightText from "../src/components/TrueWeightText/TrueWeightText";
 import { dateToFormatDate } from "../src/utilities/dateConverter";
 import OpenLogButton from "../src/components/OpenLogButton/OpenLogButton";
 import { colors } from "../src/styles/colors";
+import TrueWeightAccuracy from "../src/components/TrueWeightAccuracy/TrueWeightAccuracy";
 
 const Home = () => {
 	const currentDate = new Date();
@@ -15,6 +16,10 @@ const Home = () => {
 			</Text>
 			<View style={{ flex: 1 }}>
 				<TrueWeightText date={dateToFormatDate(currentDate)} retrospect={7} />
+				<View style={{ flexDirection: "row", justifyContent: "center" }}>
+					<Text style={{ color: colors.text.offWhite }}>Accuracy: </Text>
+					<TrueWeightAccuracy date={dateToFormatDate(currentDate)} retrospect={7} />
+				</View>
 			</View>
 			<OpenLogButton text="Open Log" date={dateToFormatDate(currentDate)} />
 			{/* TODO: Remove later - This is for testing */}
