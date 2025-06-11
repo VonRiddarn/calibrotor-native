0fadccfea06db1987dff5f3aede4e51567825669
VonRiddarn <vonriddarn@gmail.com>
2025-06-11 18:45:57 +0200

    feat: add LogModalContext so that we can dynamically open a modal with any date we want - the modal will use the LogsContext later

---
+++ src/features/LogModal/LogModalContext.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useMemo, useState } from "react";
+
+type LogModalContextType = {
+	openModal: (logDate: number) => void;
+	closeModal: () => void;
+	isVisible: boolean;
+	logDate: number | null;
+};
+
+const LogModalContext = createContext<LogModalContextType | null>(null);
+
+export const useLogModal = () => {
+	const context = useContext(LogModalContext);
+	if (!context) throw new Error("useModal must be used within a ModalProvider");
+	return context;
+};
+
+export const LogModalProvider = ({ children }: { children: React.ReactNode }) => {
+	const [isVisible, setIsVisible] = useState(false);
+	const [logDate, setLogDate] = useState<number | null>(null);
+
+	const openModal = React.useCallback((date: number) => {
+		setLogDate(date);
+		setIsVisible(true);
+	}, []);
+
+	const closeModal = React.useCallback(() => {
+		setIsVisible(false);
+	}, []);
+
+	const value = {
+		openModal,
+		closeModal,
+		isVisible,
+		logDate,
+	};
+
+	return <LogModalContext.Provider value={value}>{children}</LogModalContext.Provider>;
+};
